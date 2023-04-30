5d027028d464a5769d815e4db451ed5a3675ddae
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-04-30 12:19:24 -0400

    add meta info to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,51 +1,65 @@
-import type { LinksFunction } from '@remix-run/node';
+import type { LinksFunction, V2_MetaFunction } from '@remix-run/node';
 import {
+  isRouteErrorResponse,
   Links,
   LiveReload,
+  Meta,
   Outlet,
-  isRouteErrorResponse,
+  Scripts,
   useRouteError,
 } from '@remix-run/react';
+import type { PropsWithChildren } from 'react';
 
-import globalStylesUrl from './styles/global.css';
-import globalMediumStylesUrl from './styles/global-medium.css';
-import globalLargeStylesUrl from './styles/global-large.css';
+import globalLargeStylesUrl from '~/styles/global-large.css';
+import globalMediumStylesUrl from '~/styles/global-medium.css';
+import globalStylesUrl from '~/styles/global.css';
 
-export const links: LinksFunction = () => {
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: globalStylesUrl },
+  {
+    rel: 'stylesheet',
+    href: globalMediumStylesUrl,
+    media: 'print, (min-width: 640px)',
+  },
+  {
+    rel: 'stylesheet',
+    href: globalLargeStylesUrl,
+    media: 'screen and (min-width: 1024px)',
+  },
+];
+
+export const meta: V2_MetaFunction = () => {
+  const description = 'Learn Remix and laugh at the same time!';
+
   return [
-    {
-      rel: 'stylesheet',
-      href: globalStylesUrl,
-    },
-    {
-      rel: 'stylesheet',
-      href: globalMediumStylesUrl,
-      media: 'print, (min-width: 640px)',
-    },
-    {
-      rel: 'stylesheet',
-      href: globalLargeStylesUrl,
-      media: 'screen and (min-width: 1024px)',
-    },
+    { name: 'description', content: description },
+    { name: 'twitter:description', content: description },
+    { title: "Remix: So great, it's funny!" },
   ];
 };
 
-function Document({
-  children,
-  title = `Remix: So great, it's funny!`,
-}: {
-  children: React.ReactNode;
-  title?: string;
-}) {
+function Document({ children, title }: PropsWithChildren<{ title?: string }>) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>{title}</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="keywords" content="Remix,jokes" />
+        <meta
+          name="twitter:image"
+          content="https://remix-jokes.lol/social.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@remix_run" />
+        <meta name="twitter:site" content="@remix_run" />
+        <meta name="twitter:title" content="Remix Jokes" />
+        <Meta />
+        {title ? <title>{title}</title> : null}
         <Links />
       </head>
       <body>
         {children}
+        <Scripts />
         <LiveReload />
       </body>
     </html>
@@ -61,7 +75,8 @@ export default function App() {
 }
 
 export function ErrorBoundary() {
-  let error = useRouteError();
+  const error = useRouteError();
+  console.error(error);
 
   if (isRouteErrorResponse(error)) {
     return (
@@ -73,16 +88,15 @@ export function ErrorBoundary() {
         </div>
       </Document>
     );
-  } else if (error instanceof Error) {
-    return (
-      <Document title="Uh-oh!">
-        <div className="error-container">
-          <h1>App Error</h1>
-          <pre>{error.message}</pre>
-        </div>
-      </Document>
-    );
-  } else {
-    return <h1>Unknown Error</h1>;
   }
+
+  const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{errorMessage}</pre>
+      </div>
+    </Document>
+  );
 }
