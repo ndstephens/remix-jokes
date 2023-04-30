213fb4a0ce1f0e7712ed07fe6bc709d1b9a96381
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-04-30 12:31:09 -0400

    add meta tags

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,4 +1,4 @@
-import type { ActionArgs, LoaderArgs } from '@remix-run/node';
+import type { ActionArgs, LoaderArgs, V2_MetaFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import {
@@ -12,6 +12,21 @@ import {
 import { db } from '~/utils/db.server';
 import { getUserId } from '~/utils/session.server';
 import { requireUserId } from '~/utils/session.server';
+
+export const meta: V2_MetaFunction<typeof loader> = ({ data }) => {
+  const { description, title } = data
+    ? {
+        description: `Enjoy the "${data.joke.name}" joke and much more`,
+        title: `"${data.joke.name}" joke`,
+      }
+    : { description: 'No joke found', title: 'No joke' };
+
+  return [
+    { name: 'description', content: description },
+    { name: 'twitter:description', content: description },
+    { title },
+  ];
+};
 
 export const loader = async ({ params, request }: LoaderArgs) => {
   const userId = await getUserId(request);
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,8 @@
-import type { ActionArgs, LinksFunction } from '@remix-run/node';
+import type {
+  ActionArgs,
+  LinksFunction,
+  V2_MetaFunction,
+} from '@remix-run/node';
 import { Link, useActionData, useSearchParams } from '@remix-run/react';
 
 import stylesUrl from '~/styles/login.css';
@@ -9,6 +13,15 @@ import { createUserSession, login, register } from '~/utils/session.server';
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesUrl },
 ];
+
+export const meta: V2_MetaFunction = () => {
+  const description = 'Login to submit your own jokes to Remix Jokes!';
+  return [
+    { name: 'description', content: description },
+    { name: 'twitter:description', content: description },
+    { title: 'Remix Jokes | Login' },
+  ];
+};
 
 function validateUsername(username: unknown) {
   if (typeof username !== 'string' || username.length < 3) {
